ccbd5cd3afcce5b94dd4b9a7cd1241199b084dfe
manojkumar517 <amanojkumar517@gmail.com>
2022-12-06 09:12:44 +0530

    adding Day_6 assignments

---
+++ Day_6/range_num.sh
@@ -0,0 +1,18 @@
+echo enter m and n
+read m n
+for a in $(seq $m $n)
+do
+b=0
+for i in $(seq 2 $(expr $a - 1))
+do
+if [ $(expr $a % $i) -eq 0 ]
+then
+b=1
+break
+fi
+done
+if [ $b -eq 0 ]
+then
+echo $a
+fi
+done
